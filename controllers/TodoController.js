bb51682684dcb42a6408ca4d1a04480dee2ebbb4
BIswanath Prasad Singh <biswanathprasadsingh9@gmail.com>
2021-07-11 17:16:38 +0530

    '11Jul2021'

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -26,8 +26,81 @@ const store = (req,res) => {
     })
   })
 
+}
 
+
+
+//***VIEW***
+const view = (req,res) => {
+
+  // Todo.findById(req.params.id)
+  // .then(response=>{
+  //   res.json({
+  //     response:true,
+  //     data:response
+  //   })
+  // })
+
+  Todo.findById(req.params.id,(err,doc)=>{
+    if(!err){
+      res.json({
+        response:true,
+        data:doc
+      })
+    }else{
+      res.json({
+          response:false
+      })
+    }
+  })
 
 }
 
-module.exports={index,store};
+
+//***UPDATE***
+const update = (req,res) => {
+
+  let updatedData = {
+    name : req.body.name,
+    subject : req.body.subject,
+    gender : req.body.gender,
+    about : req.body.about,
+  }
+
+  Todo.findByIdAndUpdate(req.body._id, {$set: updatedData})
+  .then(response=>{
+    res.json({
+      response:true,
+    })
+  })
+
+  //
+  // console.log(req.body)
+  // Todo.update(req.body)
+  // .then(response=>{
+  //   res.json({
+  //     response:true
+  //   })
+  // })
+}
+
+
+//***DELETE***
+const removeid =(req,res) => {
+  Todo.findByIdAndRemove(req.params.id,(err,doc)=>{
+    if(!err){
+      res.json({
+        response:true,
+        data:doc
+      })
+    }else{
+      res.json({
+          response:false
+      })
+    }
+  })
+
+}
+
+
+module.exports={index,store,view,update,removeid};
--- routes/todo.js
+++ routes/todo.js
@@ -5,5 +5,12 @@ const TodoController = require('../controllers/TodoController');
 
 router.get('/',TodoController.index);
 router.post('/',TodoController.store);
+router.post('/update/todo',TodoController.update);
+
+router.get('/:id',TodoController.view);
+router.get('/delete/:id',TodoController.removeid);
+
+
+
 
 module.exports = router;
